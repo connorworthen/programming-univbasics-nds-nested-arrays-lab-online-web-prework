bf9aca75b8400c011ac8b5ee726c8202848b9f6b
connor worthen <connorw220@protonmail.com>
2019-11-03 20:44:39 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -45,6 +45,5 @@ def matrix_lookup(matrix, row, column)
 end
 
 def matrix_update(matrix, row, column, new_value)
-  # Update the matrix location at row and column to have the value of new_value
-  # Return the updated matrix
+  ENNET_HOUSE[0][0] = "Connor Worthen"
 end
